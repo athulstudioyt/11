dcb748e8629ed2bcfdd2f377b11bb4a07c70c71f
Athul Raj.K <133640872+athulstudioyt@users.noreply.github.com>
2024-11-05 01:57:48 +0530

    Add files via upload

---
+++ song_list.js
@@ -0,0 +1,258 @@
+//song list
+let All_song = [
+   {
+     name: "Rockaankuthu",
+     path: "music/1.mp3",
+     img: "images/1.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Dippam Dappam",
+     path: "music/2.mp3",
+     img: "images/2.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Sodakku",
+     path: "music/3.mp3",
+     img: "images/3.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Hukum - Thalaivar Alappara",
+     path: "music/4.mp3",
+     img: "images/4.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Naa Ready",
+     path: "music/5.mp3",
+     img: "images/5.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+    name: "Fear Song",
+    path: "music/6.mp3",
+    img: "images/6.jpg",
+    singer: "Anirudh Ravichander"
+   },
+      {
+
+     name: "Rockaankuthu",
+
+     path: "music/1.mp3",
+     img: "images/1.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Dippam Dappam",
+     path: "music/2.mp3",
+     img: "images/2.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Sodakku",
+     path: "music/3.mp3",
+     img: "images/3.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Hukum - Thalaivar Alappara",
+     path: "music/4.mp3",
+     img: "images/4.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Naa Ready",
+     path: "music/5.mp3",
+     img: "images/5.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+    name: "Fear Song",
+    path: "music/6.mp3",
+    img: "images/6.jpg",
+    singer: "Anirudh Ravichander"
+   },
+      {
+
+     name: "Rockaankuthu",
+
+     path: "music/1.mp3",
+     img: "images/1.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Dippam Dappam",
+     path: "music/2.mp3",
+     img: "images/2.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Sodakku",
+     path: "music/3.mp3",
+     img: "images/3.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Hukum - Thalaivar Alappara",
+     path: "music/4.mp3",
+     img: "images/4.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Naa Ready",
+     path: "music/5.mp3",
+     img: "images/5.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+    name: "Fear Song",
+    path: "music/6.mp3",
+    img: "images/6.jpg",
+    singer: "Anirudh Ravichander"
+   },
+      {
+
+     name: "Rockaankuthu",
+
+     path: "music/1.mp3",
+     img: "images/1.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Dippam Dappam",
+     path: "music/2.mp3",
+     img: "images/2.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Sodakku",
+     path: "music/3.mp3",
+     img: "images/3.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Hukum - Thalaivar Alappara",
+     path: "music/4.mp3",
+     img: "images/4.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Naa Ready",
+     path: "music/5.mp3",
+     img: "images/5.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+    name: "Fear Song",
+    path: "music/6.mp3",
+    img: "images/6.jpg",
+    singer: "Anirudh Ravichander"
+   },
+      {
+
+     name: "Rockaankuthu",
+
+     path: "music/1.mp3",
+     img: "images/1.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Dippam Dappam",
+     path: "music/2.mp3",
+     img: "images/2.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Sodakku",
+     path: "music/3.mp3",
+     img: "images/3.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Hukum - Thalaivar Alappara",
+     path: "music/4.mp3",
+     img: "images/4.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Naa Ready",
+     path: "music/5.mp3",
+     img: "images/5.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+    name: "Fear Song",
+    path: "music/6.mp3",
+    img: "images/6.jpg",
+    singer: "Anirudh Ravichander"
+   },
+      {
+
+     name: "Rockaankuthu",
+
+     path: "music/1.mp3",
+     img: "images/1.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Dippam Dappam",
+     path: "music/2.mp3",
+     img: "images/2.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Sodakku",
+     path: "music/3.mp3",
+     img: "images/3.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Hukum - Thalaivar Alappara",
+     path: "music/4.mp3",
+     img: "images/4.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+     name: "Naa Ready",
+     path: "music/5.mp3",
+     img: "images/5.jpg",
+     singer: "Anirudh Ravichander"
+   },
+   {
+    name: "Fear Song",
+    path: "music/6.mp3",
+    img: "images/6.jpg",
+    singer: "Anirudh Ravichander"
+   }
+
+];
+/*you can add more song & images from you computer*/
+
+
+/*tracks*/
+let tracks = document.querySelector('.tracks');
+
+//creating a list or generating Html
+for (let i = 0; i < All_song.length; i++) {
+
+  let Html = ` <div class="song">
+      <div class="img">
+      <img src="${All_song[i].img}"/>
+      </div>
+      <div class="more">
+      <audio src="${All_song[i].path}" id="music"></audio>
+      <div class="song_info">
+         <p id="title">${All_song[i].name}</p>
+         <p>${All_song[i].singer}</p>
+      </div>
+      <button id="play_btn"><i class="fa fa-angle-right" aria-hidden="true"></i></button>
+      </div>
+    </div>`;
+
+  tracks.insertAdjacentHTML("beforeend", Html);
+};
+
+
+/*please follow all the rules so that you do not face any problem*/
